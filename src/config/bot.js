79e14306ce23d85ed56c466fb65d383bd7cdce79
Croissant-BotOwner <yourisb@hotmail.com>
2026-07-15 12:28:41 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        name: "Croissant", // required by Discord API, not shown in the client
+        state: "Moderating Tilburg Surveillance..",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
